cd7d1ea83c994cc34c5a691af31e933ae9db908c
Datt Dongare <datt@weboniselab.com>
2012-09-11 20:17:48 +0530

    coffescript is also working now :O

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
+require "open-uri"
 class Post < ActiveRecord::Base
   attr_accessible :content
-  validate :validate_contents
+  validate :coffee_demo
 
   def validate_contents
     source = File.read(Rails.root.join("app/assets/javascripts/validations.js"))
@@ -9,5 +10,28 @@ class Post < ActiveRecord::Base
     unless context.call("validate_content_length",content)
       errors.add :content,"greater than max"
     end
+  end
+
+
+  def coffee_demo
+   #context = V8::Context.new
+   coffe = File.read(Rails.root.join("app/assets/javascripts/posts.js.coffee"))
+   ##js = CoffeeScript.compile(coffe)
+   #context.call("CoffeeScript.compile", coffe, :bare => true)
+   ##context = ExecJS.eval(js)
+   ## source = open("http://jashkenas.github.com/coffee-script/extras/coffee-script.js").read
+   ##context = ExecJS.compile(source)
+   ##context.call("CoffeeScript.compile", "square = (x) -> x * x", :bare => true)
+   #
+   ##context = ExecJS.eval(js)
+   ##context.call("validate_content_length",content)
+
+    source = open("http://jashkenas.github.com/coffee-script/extras/coffee-script.js").read
+
+    context = ExecJS.compile(source)
+    c = context.call("CoffeeScript.compile",coffe , :bare => true)
+   unless context.call(c,content)
+      errors.add :content,"greater than max"
+   end
   end
 end
