bada912df4b0daed6b7bab2df6c3c5e258dd9137
Datt Dongare <datt@weboniselab.com>
2012-09-12 12:00:20 +0530

    jquery tested, unfortunately not working

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,26 +15,15 @@ class Post < ActiveRecord::Base
 
   def coffee_demo
     @flag=1
-    logger.info "***************************************************************************befor condition #{@flag}"
+    logger.info "***************************************************************************before condition #{@flag}"
    #context = V8::Context.new
     if @flag==1
-     coffe = File.read(Rails.root.join("app/assets/javascripts/posts.js.coffee"))
-     ##js = CoffeeScript.compile(coffe)
-     #context.call("CoffeeScript.compile", coffe, :bare => true)
-     ##context = ExecJS.eval(js)
-     ## source = open("http://jashkenas.github.com/coffee-script/extras/coffee-script.js").read
-     ##context = ExecJS.compile(source)
-     ##context.call("CoffeeScript.compile", "square = (x) -> x * x", :bare => true)
-     #
-     ##context = ExecJS.eval(js)
-     ##context.call("validate_content_length",content)
-
+      coffe = File.read(Rails.root.join("app/assets/javascripts/posts.js.coffee"))
       source = File.read(Rails.root.join("app/assets/javascripts/coffee-script.js"))
-
       context = ExecJS.compile(source)
       c = context.call("CoffeeScript.compile",coffe , :bare => true)
       @flag=2
-     logger.info "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%inside condition #{@flag}"
+      logger.info "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%inside condition #{@flag}"
     end
    unless context.call(c,content)
       errors.add :content,"greater than max"
